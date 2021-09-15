f997c80b3dcd86ab04fb53ff44cb4bf72de124c2
Mateus Calza <mateushenrique17@gmail.com>
2021-09-15 20:23:28 -0300

    Script de preparo do dataset.

---
+++ dataset.py
@@ -0,0 +1,29 @@
+
+from os import makedirs
+from os import listdir
+from shutil import copyfile
+from random import seed
+from random import random
+
+dataset_home = './'
+subdirs = ['treinamento/', 'validacao/']
+for subdir in subdirs:
+	labeldirs = ['cachorros/', 'gatos/']
+	for labldir in labeldirs:
+		newdir = dataset_home + subdir + labldir
+		makedirs(newdir, exist_ok=True)
+
+seed(1)
+val_ratio = 0.25
+src_directory = 'original/'
+for file in listdir(src_directory):
+	src = src_directory + '/' + file
+	dst_dir = 'treinamento/'
+	if random() < val_ratio:
+		dst_dir = 'validacao/'
+	if file.startswith('cat'):
+		dst = dataset_home + dst_dir + 'gatos/'  + file
+		copyfile(src, dst)
+	elif file.startswith('dog'):
+		dst = dataset_home + dst_dir + 'cachorros/'  + file
+		copyfile(src, dst)
