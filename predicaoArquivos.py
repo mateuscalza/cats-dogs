0bbf7f4692986bd8c25075a7910d66dc5622ecd6
Mateus Calza <mateushenrique17@gmail.com>
2021-09-16 00:15:19 -0300

    Remoção de imports desnecessários.

--- predicaoArquivos.py
+++ predicaoArquivos.py
@@ -1,11 +1,6 @@
-from os import makedirs
 from os import listdir
-from shutil import copyfile
-from random import seed
-from random import random
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.models import load_model
 from tensorflow.keras import models
 from gui import imagem_predicao_analise
 import cv2
